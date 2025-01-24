2d5804f42c45f2a080d1fb632f523bffaf07b229
Lormen92 <lorenzo.menchella@gmail.com>
2025-01-24 17:29:12 +0100

    fix start bat comments

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
-REM Ottieni il percorso del file batch
+REM Get the batch file's directory path
 set BAT_DIR=%~dp0
 
-REM Avvia PowerShell, imposta la policy di esecuzione, esegui lo script PowerShell e termina
+REM Start PowerShell, set the execution policy, run the PowerShell script, and exit
 powershell -NoExit -ExecutionPolicy Bypass -Command "& '%BAT_DIR%coffee_script.ps1'"
